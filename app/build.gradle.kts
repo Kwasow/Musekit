b741e001ecbd3d13f01fc435d6a2462d578f7326
Karol Wąsowski <wasowski02@gmail.com>
2021-12-22 23:17:16 +0100

    Converted build.gradle to kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+fun versionCode(): Int {
+    val secondsSinceEpoch = System.currentTimeMillis() / 1000
+    // This will fail eventually, but well… It's the best we have
+    return secondsSinceEpoch.toInt()
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.kwasow.musekit"
+        minSdk = 23
+        targetSdk = 31
+        versionCode = versionCode()
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("com.github.Kwasow:BottomNavigationCircles-Android:1.1.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
