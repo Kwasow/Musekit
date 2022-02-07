6bb1f225897bc3ee153aa0e82b53a3764b9c889b
Karol Wąsowski <wasowski02@protonmail.com>
2022-02-07 19:52:30 +0100

    Disabled metronome temporarily

--- app/src/main/java/com/kwasow/musekit/adapters/MainPagerAdapter.kt
+++ app/src/main/java/com/kwasow/musekit/adapters/MainPagerAdapter.kt
@@ -4,20 +4,19 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.kwasow.musekit.fragments.ErrorFragment
-import com.kwasow.musekit.fragments.MetronomeFragment
 import com.kwasow.musekit.fragments.NoteForkFragment
-import com.kwasow.musekit.fragments.SettingsFragment
+import com.kwasow.musekit.fragments.AboutFragment
 
 class MainPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
-    private val pageCount = 3
+    private val pageCount = 2
 
     override fun getItemCount(): Int = pageCount
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> NoteForkFragment()
-            1 -> MetronomeFragment()
-            2 -> SettingsFragment()
+            // 1 -> MetronomeFragment()
+            1 -> AboutFragment()
             else -> ErrorFragment()
         }
     }
--- app/src/main/java/com/kwasow/musekit/fragments/AboutFragment.kt
+++ app/src/main/java/com/kwasow/musekit/fragments/AboutFragment.kt
@@ -2,6 +2,6 @@ package com.kwasow.musekit.fragments
 
 import androidx.fragment.app.Fragment
 
-// TODO: Settings fragment
-class SettingsFragment : Fragment() {
+// TODO: About fragment
+class AboutFragment : Fragment() {
 }
