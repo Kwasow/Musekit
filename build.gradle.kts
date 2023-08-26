0accff1c4a4ae8a1e00183b753ed16aae78e9644
Karol Wąsowski <wasowski02@protonmail.com>
2023-08-26 14:23:31 +0200

    Support language switching (Android 13+) (#4)
    This PR introduces support for per-app language switching.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = java.net.URI("https://jitpack.io") }
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath(kotlin("gradle-plugin", "1.8.21"))
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle.kts files
-    }
-}
-
-tasks {
-    val clean by registering(Delete::class) {
-        delete(rootProject.buildDir)
-    }
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
