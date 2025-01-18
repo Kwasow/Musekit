fd7e554dbd3ed441cd508c4b874d722de681c8a2
Karol Wąsowski <wasowski02@protonmail.com>
2025-01-18 12:25:47 +0100

    Included TarsosDSP as jar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("com.google.android.material:material:1.12.0")
 
+    // TarsosDSP - included as a jar archive as per the official instructions
+    implementation(files("libs/TarsosDSP-Android-2.4.jar"))
+
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
