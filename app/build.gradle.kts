89e4a69e44f59d49806ed2029ce95594f2c3e983
Karol Wąsowski <wasowski02@protonmail.com>
2025-01-19 16:47:38 +0100

    Release 2.1.1 (#46)
    Closes #44

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kwasow.musekit"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1721768583
-        versionName = "2.1.0"
+        versionCode = 1721768584
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
