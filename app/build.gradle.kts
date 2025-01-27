be3bd9b8ee4f931a81d61932b9fb1b4a605349e2
Karol Wąsowski <wasowski02@protonmail.com>
2025-01-27 13:17:49 +0100

    Bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kwasow.musekit"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1721768585
-        versionName = "2.2.0"
+        versionCode = 1721768586
+        versionName = "2.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
