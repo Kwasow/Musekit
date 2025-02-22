1c385205158572dbc6332608143c33d05e73f455
Karol Wąsowski <wasowski02@protonmail.com>
2025-02-22 19:04:35 +0100

    Added note names to notation styles

--- app/src/main/java/com/kwasow/musekit/data/NotationStyle.kt
+++ app/src/main/java/com/kwasow/musekit/data/NotationStyle.kt
@@ -1,9 +1,31 @@
 package com.kwasow.musekit.data
 
-enum class NotationStyle(val id: Int) {
-    English(0),
-    German(1),
-    FixedDo(2);
+enum class NotationStyle(val id: Int, val noteNames: List<String>) {
+    English(
+        0,
+        listOf("A", "A♯/B♭", "B", "C", "C♯/D♭", "D", "D♯/E♭", "E", "F", "F♯/G♭", "G", "G♯/A♭")
+    ),
+    German(
+        1,
+        listOf("A", "A♯/B", "H", "C", "C♯/D♭", "D", "D♯/E♭", "E", "F", "F♯/G♭", "G", "G♯/A♭")
+    ),
+    FixedDo(
+        2,
+        listOf(
+            "La",
+            "La♯/Si♭",
+            "Si",
+            "Do",
+            "Do♯/Re♭",
+            "Re",
+            "Re♯/Mi♭",
+            "Mi",
+            "Fa",
+            "Fa♯/Sol♭",
+            "Sol",
+            "Sol♯/La♭"
+        )
+    );
 
     companion object {
         fun valueOf(id: Int): NotationStyle? {
