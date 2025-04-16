059d07d73b8a5ddbd6b74867eb6f7c972979950e
Karol Wąsowski <wasowski02@protonmail.com>
2025-04-16 14:59:30 +0200

    Finished replicating metronome UI

--- app/src/main/java/com/kwasow/musekit/ui/screens/metronome/MetronomeScreen.kt
+++ app/src/main/java/com/kwasow/musekit/ui/screens/metronome/MetronomeScreen.kt
@@ -1,20 +1,30 @@
 package com.kwasow.musekit.ui.screens.metronome
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedButton
@@ -28,7 +38,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -36,6 +50,7 @@ import androidx.compose.ui.unit.sp
 import com.kwasow.musekit.R
 import com.kwasow.musekit.data.MetronomeSounds
 import org.koin.androidx.compose.koinViewModel
+import kotlin.math.max
 
 // ======= Public composables
 @Composable
@@ -49,7 +64,7 @@ fun MetronomeScreen() {
         SoundPicker()
         TempoPicker()
         BeatIndicator()
-        StartStopButton()
+        StartStopButton(this)
         AdditionalActions()
     }
 }
@@ -107,19 +122,42 @@ private fun SoundPicker() {
 private fun TempoPicker() {
     val viewModel = koinViewModel<MetronomeScreenViewModel>()
 
+    var textStyle by remember {
+        mutableStateOf(
+            TextStyle(
+                fontSize = 300.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        )
+    }
+    var readyToDraw by remember { mutableStateOf(false) }
+
     Row(
-        modifier = Modifier.padding(horizontal = 16.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         DecreaseButtons()
 
         Text(
             text = viewModel.currentTempo.toString(),
-            style = MaterialTheme.typography.displayLarge,
-            textAlign = TextAlign.Center,
+            style = textStyle,
+            maxLines = 1,
             modifier = Modifier
+                .padding(horizontal = 16.dp)
                 .weight(1f)
-                .padding(horizontal = 12.dp),
+                .drawWithContent {
+                    if (readyToDraw) drawContent()
+                },
+            onTextLayout = {
+                if (it.hasVisualOverflow) {
+                    textStyle = textStyle.copy(fontSize = textStyle.fontSize * 0.80)
+                } else {
+                    readyToDraw = true
+                }
+            }
         )
 
         IncreaseButtons()
@@ -216,19 +254,55 @@ private fun BeatIndicator() {
         valueRange = 0f..1f,
         onValueChange = {},
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 16.dp),
+            .padding(vertical = 16.dp)
+            .fillMaxWidth(),
         enabled = false,
         interactionSource = remember { MutableInteractionSource() }
     )
 }
 
 @Composable
-private fun StartStopButton() {
-
+private fun StartStopButton(scope: ColumnScope) {
+    with(scope) {
+        Box(modifier = Modifier.weight(1f)) {
+            Card(
+                modifier = Modifier
+                    .size(50.dp)
+            ) {}
+        }
+    }
 }
 
 @Composable
 private fun AdditionalActions() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Button(
+            onClick = {},
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_edit),
+                contentDescription = "",
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text(text = stringResource(id = R.string.set_beat))
+        }
 
+        Button(
+            onClick = {},
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_tap),
+                contentDescription = "",
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text(text = stringResource(id = R.string.tap_beat))
+        }
+    }
 }
