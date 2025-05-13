f550c2211f02736d94fbeac5bace63f1032b79f3
Karol Wąsowski <wasowski02@protonmail.com>
2025-05-13 23:02:27 +0200

    Added pitch selector

--- app/src/main/java/com/kwasow/musekit/ui/screens/fork/NoteForkAutoScreen.kt
+++ app/src/main/java/com/kwasow/musekit/ui/screens/fork/NoteForkAutoScreen.kt
@@ -1,11 +1,20 @@
 package com.kwasow.musekit.ui.screens.fork
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -15,11 +24,13 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.kwasow.musekit.R
+import com.kwasow.musekit.ui.components.AutoSizeText
 import com.kwasow.musekit.ui.components.PermissionMicrophoneView
 import com.kwasow.musekit.ui.components.TunerView
 import com.kwasow.musekit.utils.MusekitPitchDetector
@@ -72,6 +83,59 @@ private fun MainView() {
                 },
             cents = detectionResult.value?.second,
         )
+
+        PitchSelector(
+            modifier = Modifier.align(Alignment.BottomCenter),
+        )
+    }
+}
+
+@Composable
+private fun PitchSelector(modifier: Modifier = Modifier) {
+    val viewModel = koinViewModel<NoteForkScreenViewModel>()
+    val automaticTunerPitch by viewModel.automaticTunerPitch.collectAsState(440)
+
+    Card(modifier = modifier.fillMaxWidth()) {
+        Row(
+            modifier =
+                Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Max),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_minus_circle),
+                contentDescription = stringResource(id = R.string.contentDescription_pitch_down),
+                tint = MaterialTheme.colorScheme.primary,
+                modifier =
+                    Modifier
+                        .fillMaxHeight()
+                        .padding(8.dp)
+                        .clickable {
+                            viewModel.setAutomaticTunerPitch(automaticTunerPitch - 1)
+                        },
+            )
+
+            AutoSizeText(
+                text = stringResource(id = R.string.pitch_placeholder, automaticTunerPitch),
+                modifier = Modifier.fillMaxHeight(),
+            )
+
+            Icon(
+                painter = painterResource(id = R.drawable.ic_plus_circle),
+                contentDescription = stringResource(id = R.string.contentDescription_pitch_up),
+                tint = MaterialTheme.colorScheme.primary,
+                modifier =
+                    Modifier
+                        .fillMaxHeight()
+                        .padding(8.dp)
+                        .clickable {
+                            viewModel.setAutomaticTunerPitch(automaticTunerPitch + 1)
+                        },
+            )
+        }
     }
 }
 
