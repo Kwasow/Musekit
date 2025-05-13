9a38a6c6e91164389f70c69730abf97476736208
Karol Wąsowski <wasowski02@protonmail.com>
2025-05-13 12:56:29 +0200

    General idea for play pause button animation

--- app/src/main/java/com/kwasow/musekit/ui/components/PlayPauseButton.kt
+++ app/src/main/java/com/kwasow/musekit/ui/components/PlayPauseButton.kt
@@ -1,13 +1,26 @@
 package com.kwasow.musekit.ui.components
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import androidx.graphics.shapes.CornerRounding
+import androidx.graphics.shapes.Morph
+import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.star
+import com.kwasow.musekit.utils.MorphPolygonShape
 
 // ====== Public composables
 @Composable
@@ -32,17 +45,10 @@ fun PlayPauseButton(
     onPause: () -> Unit,
 ) {
     Box(modifier = modifier) {
-        Card(
-            modifier =
-                Modifier
-                    .size(50.dp)
-                    .clickable {
-                        if (isPlaying) {
-                            onPause()
-                        } else {
-                            onPlay()
-                        }
-                    },
+        MorphingContainer(
+            state = isPlaying,
+            onPlay = onPlay,
+            onPause = onPause,
         ) {
             if (isPlaying) {
                 Text("Playing")
@@ -50,5 +56,55 @@ fun PlayPauseButton(
                 Text("Paused")
             }
         }
+    }
+}
+
+// ====== Private composables
+@Composable
+private fun MorphingContainer(
+    state: Boolean,
+    onPlay: () -> Unit,
+    onPause: () -> Unit,
+    content: @Composable () -> Unit,
+) {
+    val shapeA = remember {
+        RoundedPolygon(
+            numVertices = 6,
+            rounding = CornerRounding(0.2f)
+        )
+    }
+    val shapeB = remember {
+        RoundedPolygon.star(
+            numVerticesPerRadius = 12,
+            rounding = CornerRounding(0.1f),
+            radius = 1f,
+            innerRadius = 0.9f,
+        )
+    }
+    val morph = remember { Morph(shapeA, shapeB) }
+
+    val animatedProgress = animateFloatAsState(
+        targetValue = if (state) 1f else 0f,
+        label = "progress",
+        animationSpec = spring(dampingRatio = 0.4f, stiffness = Spring.StiffnessMedium)
+    )
+
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .padding(8.dp)
+            .clip(MorphPolygonShape(morph, animatedProgress.value))
+            .background(MaterialTheme.colorScheme.secondary)
+            .size(200.dp)
+            .clickable {
+                if (state) {
+                    onPause()
+                } else {
+                    onPlay()
+                }
+            },
+        contentAlignment = Alignment.Center,
+    ) {
+        content()
     }
 }
---
+++ app/src/main/java/com/kwasow/musekit/utils/MorphPolygonShape.kt
@@ -0,0 +1,33 @@
+package com.kwasow.musekit.utils
+
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Matrix
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.graphics.shapes.Morph
+import androidx.graphics.shapes.toPath
+
+class MorphPolygonShape(
+    private val morph: Morph,
+    private val percentage: Float
+) : Shape {
+    private val matrix = Matrix()
+
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        // Below assumes that you haven't changed the default radius of 1f, nor the centerX and centerY of 0f
+        // By default this stretches the path to the size of the container, if you don't want stretching, use the same size.width for both x and y.
+        matrix.scale(size.width / 2f, size.height / 2f)
+        matrix.translate(1f, 1f)
+
+        val path = morph.toPath(progress = percentage).asComposePath()
+        path.transform(matrix)
+        return Outline.Generic(path)
+    }
+}
