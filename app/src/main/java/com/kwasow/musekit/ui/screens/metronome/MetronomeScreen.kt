d53f478a7a26ec1d7d1adaec3c121690b6ea1996
Karol Wąsowski <wasowski02@protonmail.com>
2025-05-17 12:05:45 +0200

    Simplified metronome UI

--- app/src/main/java/com/kwasow/musekit/ui/screens/metronome/MetronomeScreen.kt
+++ app/src/main/java/com/kwasow/musekit/ui/screens/metronome/MetronomeScreen.kt
@@ -60,15 +60,12 @@ fun MetronomeScreen() {
     val metronomeService =
         rememberBoundLocalService<MetronomeService, MetronomeService.LocalBinder> { service }
 
-    when (metronomeService) {
-        null -> LoadingView()
-        else -> MainView(service = metronomeService)
-    }
+    MainView(service = metronomeService)
 }
 
 // ====== Private composables
 @Composable
-private fun MainView(service: MetronomeService) {
+private fun MainView(service: MetronomeService?) {
     val viewModel = koinViewModel<MetronomeScreenViewModel>()
     val bpm by viewModel.metronomeBpm.collectAsState(60)
 
@@ -98,14 +95,6 @@ private fun MainView(service: MetronomeService) {
     }
 }
 
-@Composable
-private fun LoadingView() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        CircularProgressIndicator(
-            modifier = Modifier.align(Alignment.Center),
-        )
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -243,11 +232,11 @@ private fun ChangeButtons(
 }
 
 @Composable
-private fun BeatIndicator(service: MetronomeService) {
-    val sliderPosition = service.tickerPosition.observeAsState()
+private fun BeatIndicator(service: MetronomeService?) {
+    val sliderPosition = service?.tickerPosition?.observeAsState()
 
     Slider(
-        value = sliderPosition.value!!,
+        value = sliderPosition?.value ?: 0f,
         steps = 0,
         valueRange = 0f..1f,
         onValueChange = {},
@@ -267,16 +256,16 @@ private fun BeatIndicator(service: MetronomeService) {
 
 @Composable
 private fun PlayPause(
-    service: MetronomeService,
+    service: MetronomeService?,
     scope: ColumnScope,
 ) {
-    val isPlaying = service.isPlaying.observeAsState()
+    val isPlaying = service?.isPlaying?.observeAsState()
 
     with(scope) {
         PlayPauseButton(
             modifier = Modifier.weight(1f),
-            isPlaying = isPlaying.value == true,
-            onChange = { service.startStopMetronome() },
+            isPlaying = isPlaying?.value == true,
+            onChange = { service?.startStopMetronome() },
         )
     }
 }
