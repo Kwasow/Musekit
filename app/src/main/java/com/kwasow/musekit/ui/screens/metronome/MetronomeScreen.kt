702fc8fdb107d0033f143ffe43ae2b8141e51899
Karol Wąsowski <wasowski02@protonmail.com>
2025-10-02 15:52:34 +0200

    Finished adjusting Metronome UI

--- app/src/main/java/com/kwasow/musekit/ui/screens/metronome/MetronomeScreen.kt
+++ app/src/main/java/com/kwasow/musekit/ui/screens/metronome/MetronomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
@@ -35,6 +36,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kwasow.musekit.R
@@ -104,7 +106,7 @@ private fun MainView(
         modifier =
             Modifier
                 .fillMaxSize()
-                .padding(16.dp),
+                .padding(horizontal = 16.dp),
     ) {
         TopBar()
 
@@ -153,11 +155,13 @@ private fun TempoDisplay() {
                 append("♪ ${stringResource(id = R.string.tempo)} = ")
             }
             append(currentTempo?.toString() ?: "")
+            append("60")
         }
 
     Text(
         text = text,
         fontSize = 20.sp,
+        modifier = Modifier.padding(8.dp)
     )
 }
 
@@ -216,4 +220,10 @@ private fun PlayPause(
             }
         },
     )
+}
+
+@Preview
+@Composable
+private fun TopBarPreview() {
+    TopBar()
 }
